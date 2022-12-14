eb40a86feb5300f3c2a986935f386df15382d007
JesusPlasencia <jesusplasencia2508@hotmail.com>
2022-12-14 00:16:30 -0500

    dateTimePicker

--- app/src/main/java/idat/dami/prueba/view/fragment/ListarHistorialFragment.kt
+++ app/src/main/java/idat/dami/prueba/view/fragment/ListarHistorialFragment.kt
@@ -1,27 +1,38 @@
 package idat.dami.prueba.view.fragment
 
+import android.app.DatePickerDialog
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.DatePicker
+import androidx.fragment.app.DialogFragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import idat.dami.prueba.R
 import idat.dami.prueba.databinding.FragmentListarHistorialBinding
 import idat.dami.prueba.retrofit.AzSessionManager
 import idat.dami.prueba.utilitarios.MiApp
 import idat.dami.prueba.view.adapters.HistorialAdapter
 import idat.dami.prueba.viewmodel.HistorialViewModel
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.logging.SimpleFormatter
 
-class ListarHistorialFragment : Fragment() {
+class ListarHistorialFragment : Fragment(), View.OnClickListener, DatePickerDialog.OnDateSetListener {
 
-
+    private val calendar = Calendar.getInstance();
+    private val formatter = SimpleDateFormat("dd-MM-YYYY", Locale.ENGLISH)
     private var _binding : FragmentListarHistorialBinding? = null
     private val binding get() = _binding!!
     private lateinit var historialViewModel: HistorialViewModel
     private var sessionManager = AzSessionManager(MiApp.applicationContext)
     var token: String? = "";
+    var fecha: String = "";
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -33,18 +44,45 @@ class ListarHistorialFragment : Fragment() {
             requireActivity())
         historialViewModel = ViewModelProvider(requireActivity())
             .get(HistorialViewModel::class.java)
-
         token = sessionManager.fetchAuthToken();
-        listarHistorial(token)
-
+        binding.edtFecha.setOnClickListener{
+            DatePickerDialog(
+                requireActivity(),
+                this,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+                ).show()
+        }
+        binding.btnHistorial.setOnClickListener(this)
         return binding.root
     }
 
     fun listarHistorial(tokenAuth: String?){
-        historialViewModel.listarHistorial(tokenAuth,"30-11-2022").observe(
+        fecha = binding.edtFecha.text.toString();
+        Log.println(Log.DEBUG, "FECHA: ", fecha)
+        historialViewModel.listarHistorial(tokenAuth,fecha).observe(
             viewLifecycleOwner, Observer {
                     response -> binding.rvHistorial.adapter = HistorialAdapter(response)
             }
         )
     }
+
+    override fun onClick(view: View) {
+        when(view.id) {
+            R.id.btnHistorial -> {
+                listarHistorial(token)
+            }
+        }
+    }
+
+    override fun onDateSet(view: DatePicker, year: Int, month: Int, day: Int) {
+        calendar.set(year, month, day);
+        displayFormattedDate(calendar.timeInMillis)
+    }
+
+    private fun displayFormattedDate(timestamp: Long) {
+        binding.edtFecha.text = formatter.format(timestamp);
+    }
+
 }
